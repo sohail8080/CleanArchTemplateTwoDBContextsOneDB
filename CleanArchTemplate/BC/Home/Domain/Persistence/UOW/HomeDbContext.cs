02420faf86195f6f9925d220b4a7fb8a8bd6b3da
M. Sohail <sohail8080@hotmail.com>
2019-09-16 21:25:13 +0500

    Done

---
+++ CleanArchTemplate/BC/Home/Domain/Persistence/UOW/HomeDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using CleanArchTemplate.BC.AccessControl.Account.Domain.Models;
+//using CleanArchTemplate.BC.AccessControl.Home.Domain.Models;
+//using CleanArchTemplate.BC.AccessControl.Manage.Domain.Models;
+
+namespace CleanArchTemplate.BC.Home.Domain.Persistence.UOW
+{
+    //public class HomeDbContext : IdentityDbContext<ApplicationUser>
+    //{
+    //    public HomeDbContext()
+    //        : base("HomeDbContext", throwIfV1Schema: false)
+    //    {
+    //    }
+
+    //    public static HomeDbContext Create()
+    //    {
+    //        return new HomeDbContext();
+    //    }
+    //}
+}
